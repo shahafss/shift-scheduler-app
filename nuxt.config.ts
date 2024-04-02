daeb0c365552f5f6d7a53940f1b2d0f74d1d885f
Shahaf Shumel <shahaflidor@gmail.com>
2024-04-02 20:24:42 +0300

    revert

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,2 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  target: "static",
-  router: {
-    base: "/shift-scheduler-app/", // Replace 'your-repo-name' with your GitHub repository name
-  },
-});
+export default defineNuxtConfig({ devtools: { enabled: true } });
