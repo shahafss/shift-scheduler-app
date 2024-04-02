34141ccfa1ad9c67fc5ce97cc466e0bb09a43b2c
Shahaf Shumel <shahaflidor@gmail.com>
2024-04-02 21:35:00 +0300

    re-try setting router config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({ devtools: { enabled: true } });
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  router: {
+    base: "/shift-scheduler-app/", // Replace with your details
+  },
+});
