3e657be0c48f0a1d2678fbb40011542fd46c2e3d
Shahaf Shumel <shahaflidor@gmail.com>
2024-04-02 22:36:06 +0300

    update nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
+/* eslint-disable indent */
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  experimental: {
+    payloadExtraction: true,
+  },
   router: {
     base: "/shift-scheduler-app/", // Replace with your details
   },
