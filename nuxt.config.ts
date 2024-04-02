861afff4ccd87cd7723b91344427ba6abab1deb3
Shahaf Shumel <shahaflidor@gmail.com>
2024-04-02 14:50:04 +0300

    try fix deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  target: "static",
   router: {
     base: "/shift-scheduler-app/", // Replace 'your-repo-name' with your GitHub repository name
   },
