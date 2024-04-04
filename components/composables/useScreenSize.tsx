e03ad0ed255eb9cd8dc2fe4abb8372e4225a622b
Shahaf Shumel <shahaflidor@gmail.com>
2024-04-05 02:33:52 +0300

    remove debounce

--- components/composables/useScreenSize.tsx
+++ components/composables/useScreenSize.tsx
@@ -1,17 +1,16 @@
 import { ref, onMounted, onBeforeUnmount } from "vue"
-import { debounce } from "lodash"
 
 export function useScreenSize() {
   const isSsize = ref(false)
   const isMsize = ref(false)
   const isLsize = ref(false)
 
-  const handleResize = debounce(() => {
+  const handleResize = () => {
     const width = window.innerWidth
     isSsize.value = width < 600
     isMsize.value = width >= 600 && width < 1024
     isLsize.value = width >= 1024
-  }, 200)
+  }
 
   onMounted(() => {
     handleResize()
